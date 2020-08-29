23f2b28a72d50d258f0fc015d39b61f7a6cf3c12
Abhinand017 <69962165+Abhinand017@users.noreply.github.com>
2020-08-29 12:38:24 +0530

    Add files via upload

---
+++ radius and perimeter.py
@@ -0,0 +1,15 @@
+from math import pi
+radius = int(input())
+
+def perim(k):
+	#your code goes here
+	x=2*pi*k
+	return x
+
+def area():
+	#your code goes here
+	y=pi*(radius *radius )
+	return y
+	
+print("Perimeter:", round(perim(radius), 1))
+print("Area:",round(area(), 2))
